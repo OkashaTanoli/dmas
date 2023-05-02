de8ea4464a744ae46cddbae76c59ad466fdb5d82
okashatanoli12345@gmail.com <64710012+OkashaTanoli@users.noreply.github.com>
2023-05-02 16:54:05 +0500

    add member responsiveness added

--- app/addmember/page.tsx
+++ app/addmember/page.tsx
@@ -5,8 +5,8 @@ function AddMember() {
     return (
         <div>
             <PageHeading text='Add Member' />
-            <div className='w-[1100px] m-auto mb-20'>
-                <div className='w-[500px] m-auto box_shadow'>
+            <div className='w-[1100px] xl-lg:w-[90%] m-auto mb-20'>
+                <div className='w-[500px] sm:w-full m-auto box_shadow'>
                     <h1 className='text-xl text-zinc-800 font-bold py-3 px-5 border-b border-zinc-300'>Add Member</h1>
                     <div className='mt-5 px-5 pb-5'>
                         <input type="text" placeholder='Enter Your Name' className='py-2 px-5 text-sm mb-3 text-zinc-800 border border-zinc-300 w-full rounded-lg bg-zinc-50' id="" />
